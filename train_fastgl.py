780feb5e11bf0319d67ee7351f40692972e73047
Anonymous <131567093+a1bc2def6g@users.noreply.github.com>
2024-06-03 15:57:30 +0800

    Update train_fastgl.py

--- train_fastgl.py
+++ train_fastgl.py
@@ -480,7 +480,7 @@ if __name__ == '__main__':
     parser.add_argument("--is-reorder",type=bool,default=False,)
     parser.add_argument("--is-presample",type=bool,default=True,)
     parser.add_argument("--dataset_name",type=str,default='reddit',)
-    parser.add_argument("--dataset_path",type=str,default='dataset',)
+    parser.add_argument("--dataset_path",type=str,default='../dataset',)
     parser.add_argument("--log_path",type=str,default='logs_fig9/',)
     args = parser.parse_args()
     setup_seed(12345)
